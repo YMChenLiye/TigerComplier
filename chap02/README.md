a4ed32b099bd4439de107df63228f1332beb0a23
liyechen <ymchenliye@gmail.com>
2018-04-07 18:10:39 +0800

    搭建框架

---
+++ chap02/README.md
@@ -0,0 +1,6 @@
+# Usage:
+* flex tiger.lex
+* mkdir build
+* cd build
+* cmake ..
+* make
---
+++ chap02/util.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "util.h"
+
+void * checked_malloc(int len)
+{
+    void *p = malloc(len);
+    if(!p)
+    {
+        fprintf(stderr, "\nRun out of memory!\n");
+        exit(1);
+    }
+    return p;
+}

baafccd632739e99dbba848b5d65cc302d5931e2
liyechen <ymchenliye@gmail.com>
2018-07-15 16:43:13 +0800

    change format shell

--- format-code.sh
+++ format-code.sh
@@ -2,6 +2,18 @@ for dir in ./*
 do
 	if test -d $dir
 	then
-		find $dir -regex ".*\.[hc]" | xargs clang-format -i -style=file 
+		for file in $dir/*
+		do
+			if test -f $file
+			then
+				if [ "${file##*.}"x = "h"x ]||[ "${file##*.}"x = "c"x ];	
+				then
+					#do something
+					echo "format " $file
+					clang-format -i -style=file $file
+				fi
+			fi
+		done
+		#find $dir -regex ".*\.[hc]" | xargs clang-format -i -style=file
 	fi
 done
